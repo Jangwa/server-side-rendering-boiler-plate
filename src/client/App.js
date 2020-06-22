415e4970a7485d1f70f02ba4d495996832d59b47
Jangwa <akachari@gmail.com>
2020-06-23 00:07:12 +0530

    CSS

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {render} from 'react-dom';
 
-const App = () => <div onClick={() => console.log("working")}>Working1</div>;
+const styleObj = {
+  "display": "flex",
+  "justify-content": "center",
+  "align-items": "center",
+  "height": "100vh",
+  "flex-direction": "column"
+};
+const buttonStyleObj = {
+  "border":"solid 3px #01a9b4",
+  "background":"#086972",
+  "padding":"10px 50px",
+  "border-radius":"5px",
+  "color":"#fff",
+  "font-family":"sans-serif",
+  "font-weight":"bolder",
+  "font-size":"20px"
+};
+
+const App = () => {
+  const [welcomeText, setWelcomeText] = useState("");
+  return(
+    <div style={styleObj}>
+      <button style={buttonStyleObj} onClick={() => setWelcomeText("Events are successfully attached on server side rendered HTML.")}>Click</button>
+      <h1>{welcomeText}</h1>
+    </div>
+  )
+};
 
 export default App;
